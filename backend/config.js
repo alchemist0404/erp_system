8c05ee30c1df3c4c72dde14be83c8fce6d258f28
maletiger <maletiger000@gmail.com>
2021-08-19 22:33:40 +0300

    update server

--- backend/config.js
+++ backend/config.js
@@ -1,5 +1,5 @@
 module.exports = {
 	testdb: "mongodb://localhost:27017/games?serverSelectionTimeoutMS=5000&connectTimeoutMS=10000",
-	livedb: "mongodb://199.192.17.41/:27017/games?serverSelectionTimeoutMS=5000&connectTimeoutMS=10000",
+	livedb: "mongodb://actiongame:actiongame123$@localhost:27731/actiongame?serverSelectionTimeoutMS=5000&connectTimeoutMS=10000&authSource=admin&authMechanism=SCRAM-SHA-256",
 	DIR: __dirname
 }
--- backend/src/configuration/mongoose.js
+++ backend/src/configuration/mongoose.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose")
 const config = require("../../config")
 
 const mongooseConfig = () => {
-    mongoose.connect(config.testdb, { useNewUrlParser: true ,useFindAndModify: false,useUnifiedTopology: true,useCreateIndex : true}).then(() => {
+    mongoose.connect(process.env.NODE_ENV === "production" ? config.livedb : config.testdb, { useNewUrlParser: true ,useFindAndModify: false,useUnifiedTopology: true,useCreateIndex : true}).then(() => {
         console.log('Database is connected');
     })
 }
